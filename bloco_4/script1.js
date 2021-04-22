6d540eea526b9581665152eb03542201faf61c3b
Abner <gedeaoabner@gmail.com>
2021-04-22 18:19:11 -0300

    cria pasta do modulo 4 e adiciona exercicio 1

---
+++ bloco_4/script1.js
@@ -0,0 +1,11 @@
+const name = "Gedeão Abner";
+const birthCity = "Rio de Janeiro";
+let birthYear = "1994";
+
+birthYear = "2030";
+
+//birthCity = "São Paulo";
+
+console.log(name);
+console.log(birthCity);
+console.log(birthYear);
