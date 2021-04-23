56c0dfcabb2ea784ea16df95a72651e7ae9906bf
Abner <gedeaoabner@gmail.com>
2021-04-23 18:55:36 -0300

    adiciona arquivo exercicio6.js com lista de exercicios pós-aula no dia 4.2

---
+++ bloco_4/dia2/exercicio6.js
@@ -0,0 +1,8 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// exercicio 1
+for ( let contador = 0; contador < numbers.length; contador += 1) {
+    console.log(numbers[contador]);
+}
+
+//exercicio 2
