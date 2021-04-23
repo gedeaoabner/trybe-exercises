d3aebf0dfc880eae7e80a5a7e92f4b8176c35bab
Abner <gedeaoabner@gmail.com>
2021-04-23 19:29:39 -0300

    atualiza arquivos adicionando exercicios do 6 ao 8

--- bloco_4/dia2/lista-de-exercicios-pos-aula.js
+++ bloco_4/dia2/lista-de-exercicios-pos-aula.js
@@ -45,3 +45,34 @@ for (let index = 0; index < numbers.length; index += 1) {
     }
 }
 console.log(maiorNumero);
+
+// exercicio 6 
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let isOdd = 0;
+for (let index = 0; index < numbers.length; index += 1) {
+    if ((numbers[index] % 2) > 0) {
+        isOdd += 1;
+    }
+}
+if (isOdd < 0) {
+    console.log(isOdd);
+} else {
+    console.log("nenhum valor ímpar encontrado");
+}
+
+// exercico 7 
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let menorValor = 999;
+for (let index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] < menorValor) {
+        menorValor = numbers[index];
+    }
+}
+console.log(menorValor);
+
+// exercicio 8 
+let novoArray = [];
+for (let index = 1; index <= 25; index += 1) {
+    novoArray.push(index);
+}
+console.log(novoArray);
