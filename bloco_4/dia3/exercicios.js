965e74c5fe4cc2bb2aced55e34ea43e414b1274e
Abner <gedeaoabner@gmail.com>
2021-04-26 16:07:25 -0300

    adiciona resolução da questão 2

--- bloco_4/dia3/exercicios.js
+++ bloco_4/dia3/exercicios.js
@@ -1,9 +1,27 @@
 // exercicio 1
 let valor = 3;
 
-if (valor > 0) {
+if (valor > 1) {
     for (index = 0; index < valor; index += 1) {
         console.log("****");
     }
 }
 
+// exercico 2
+
+let valor = 5;
+let asterisco = ["*", "**", "***", "****", "*****"];
+if (valor > 1) {
+    for (index = 0; index < valor; index += 1) {
+        console.log(asterisco[index]);
+    }
+}
+
+let valor = 5;
+let asterisco = "*";
+let linha = "";
+
+for (index = 0; index < valor; index += 1) {
+    console.log(asterisco);
+    asterisco = asterisco + "*";
+}
