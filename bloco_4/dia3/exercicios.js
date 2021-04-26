5f94ee093b2bf1c413d9697665b1635d408dde95
Abner <gedeaoabner@gmail.com>
2021-04-26 18:21:31 -0300

    adiciona tentativa de resoluçao do exercicio 3 e o gabarito com a resoluçao correta

--- bloco_4/dia3/exercicios.js
+++ bloco_4/dia3/exercicios.js
@@ -19,9 +19,44 @@ if (valor > 1) {
 
 let valor = 5;
 let asterisco = "*";
-let linha = "";
 
 for (index = 0; index < valor; index += 1) {
     console.log(asterisco);
     asterisco = asterisco + "*";
 }
+
+// exercicio 3 
+
+let valor = 5;
+let asterisco = "*";
+let linha = "";
+
+for (index = 1; index < (valor + 1); index += 1) {
+    for (let espaco = (valor - index); espaco > 0; espaco -= 1) {
+        asterisco = linha;
+        //console.log("oi");
+    };
+};
+
+
+// gabarito exercicio 3
+
+let valor = 5;
+let asterisco = "*";
+let linha = "";
+let posicao = valor;
+
+for (indexLinha = 0; indexLinha < valor; indexLinha += 1) {
+    for (let indexColuna = 0; indexColuna <= valor; indexColuna += 1) {
+        if (indexColuna < posicao) {
+            linha = linha + " ";
+        } else {
+            linha = linha + asterisco;
+        }
+    };
+    console.log(linha);
+    linha = "";
+    posicao -= 1;
+};
+
+
