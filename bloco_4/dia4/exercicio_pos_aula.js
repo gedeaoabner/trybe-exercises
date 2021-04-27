77df55bfdf5af467b5df52cfd51eaafd8adb15dc
Abner <gedeaoabner@gmail.com>
2021-04-27 18:59:32 -0300

    adiciona tentativa de resolução do exercicio parte 1 com o gabarito correto

--- bloco_4/dia4/exercicio_pos_aula.js
+++ bloco_4/dia4/exercicio_pos_aula.js
@@ -3,3 +3,39 @@ let info = {
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
+
+info.recorrente = "Sim";
+
+let info2 = {
+    personagem: 'TIo Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+    recorrente: "Sim"
+};
+
+//console.log("Bem-vinda, " + info.personagem);
+//console.log(info);
+
+// for (let key in info) {
+//     console.log(key);
+// };
+
+// for (let value in info) {
+//     console.log(info[value]);
+// };
+
+// for (let value in info) {
+//     for (let value2 in info2) {
+//         console.log(info[value] + " e " + info2[value]);
+//     };
+// ;} 
+
+// GABARITO
+
+for (let key in info) {
+    if (key === "recorrente" && info.recorrente === "Sim" && info2.recorrente === "Sim") {
+        console.log("Ambos recorrentes");
+    } else {
+        console.log(info[key] + " e " + info2[key]);
+    }
+}
