f459a02b4e7cebf0188ddc8abaa674bf39cab789
Abner <gedeaoabner@gmail.com>
2021-05-03 19:11:03 -0300

    cria estrutura de repetição para adicionar os dias no calendário

--- bloco_5/dia3/exercicio-pos-aula/script.js
+++ bloco_5/dia3/exercicio-pos-aula/script.js
@@ -11,6 +11,17 @@ function createDaysOfTheWeek() {
   };
 };
 
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+let dias = document.querySelector("#days");
+console.log(dias);
+for (let index = 0; index < dezDaysList.length; index += 1) {
+  let hoje = dezDaysList[index];
+  let criacao = document.createElement("li");
+  criacao.innerText = hoje;
+  dias.appendChild(criacao);
+}
+
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
