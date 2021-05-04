08705db87b4e65a4476703f14bbf12c397c1299c
Abner <gedeaoabner@gmail.com>
2021-05-04 04:02:40 -0300

    adiciona botão Feriado (exercicio 2)

--- bloco_5/dia3/exercicio-pos-aula/script.js
+++ bloco_5/dia3/exercicio-pos-aula/script.js
@@ -13,24 +13,36 @@ function createDaysOfTheWeek() {
 
 const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
-let dias = document.querySelector("#days");
-console.log(dias);
-for (let index = 0; index < dezDaysList.length; index += 1) {
-  let hoje = dezDaysList[index];
-  let criacao = document.createElement("li");
-  criacao.setAttribute("class", "day");
-  if (hoje === 24 || hoje === 25 || hoje === 31) {
-    // console.log("alow");
-    criacao.className += " holiday";
-    //criacao.setAttribute("class", "holiday");
+function diasDoMes() {
+  let dias = document.querySelector("#days");
+  console.log(dias);
+  for (let index = 0; index < dezDaysList.length; index += 1) {
+    let hoje = dezDaysList[index];
+    let criacao = document.createElement("li");
+    criacao.setAttribute("class", "day");
+    if (hoje === 24 || hoje === 25 || hoje === 31) {
+      criacao.className += " holiday";
+    }
+    if (hoje === 4 || hoje === 11 || hoje === 18 || hoje === 25) {
+      criacao.className += " friday";
+    }
+    criacao.innerText = hoje;
+    dias.appendChild(criacao);
   }
-  if (hoje === 4 || hoje === 11 || hoje === 18 || hoje === 25) {
-    criacao.className += " friday";
-  }
-  criacao.innerText = hoje;
-  dias.appendChild(criacao);
 }
 
+function isFeriado(feriado) {
+  let task = document.querySelector(".tasks-container");
+  console.log(task);
+  let botao = document.createElement("button");
+  botao.setAttribute("id", "btn-holiday");
+  botao.innerText = feriado;
+  let lugarBotao = document.querySelector(".buttons-container");
+  lugarBotao.appendChild(botao);
+}
+
+isFeriado("Feriado");
 createDaysOfTheWeek();
+diasDoMes();
 
 // Escreva seu código abaixo.
