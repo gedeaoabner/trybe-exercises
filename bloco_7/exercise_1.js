42f9fd21c4481ee984eb5f63d574cccc8de41952
Abner <gedeaoabner@gmail.com>
2021-05-19 19:19:33 -0300

    adiciona exercicio 2 completo

--- bloco_7/exercise_1.js
+++ bloco_7/exercise_1.js
@@ -27,3 +27,4 @@ function testingScope(escopo) {
   }
 
   testingScope(true);
+  
---
+++ bloco_7/exercise_2.js
@@ -0,0 +1,7 @@
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+oddsAndEvens.sort(function(a, b) {
+  return a - b;
+});
+
+console.log(oddsAndEvens);
