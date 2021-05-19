994d9a7604ae05f944bdce356e8873d6c29185ba
Abner <gedeaoabner@gmail.com>
2021-05-19 16:07:52 -0300

    melhora resolução do exercicio 1 e adiciona comentário com o enunciado

--- bloco_7/exercise_1.js
+++ bloco_7/exercise_1.js
@@ -1,3 +1,7 @@
+// Faça as modificações necessárias na função para que o seu comportamento respeite o escopo no qual cada variável foi declarada.
+// > Modifique a estrutura da função para que ela seja uma arrow function .
+// > Modifique as concatenações para template literals .
+// 
 // function testingScope(escopo) {
 //     if (escopo === true) {
 //       var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
@@ -15,8 +19,7 @@
 function testingScope(escopo) {
     if (escopo === true) {
       let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-      ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
-      console.log(ifScope);
+      console.log(`${ifScope} ótimo, fui utilizada no escopo !`);
     } else {
       let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
       console.log(elseScope);
