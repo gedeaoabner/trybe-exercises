790837ed6fb1e12312061475a010542794990a00
Abner <gedeaoabner@gmail.com>
2021-05-20 15:27:46 -0300

    adiciona a parte 3 dos testes

--- bloco_7/testes.js
+++ bloco_7/testes.js
@@ -86,3 +86,31 @@ const customer1 = {
   
   console.log('Estudante 2');
   listSkills(student2);
+
+  // Testes parte 3
+
+  const student = {
+    Html: 'Muito Bom',
+    Css: 'Bom',
+    JavaScript: 'Ótimo',
+    SoftSkill: 'Ótimo',
+  };
+  
+  const listSkillsValuesWithFor = (student) => {
+    const skills = [];
+    for(skill in student) {
+      skills.push(student[skill]);
+    }
+  
+    return skills;
+  };
+  
+  const listSkillsValuesWithValues = (student) => Object.values(student);
+  
+  // Sem Object.values
+  console.log(listSkillsValuesWithFor(student));
+  
+  // Com Object.values
+  console.log(listSkillsValuesWithValues(student));
+
+  // Testes parte 4
