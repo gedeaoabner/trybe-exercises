17f28fce6a1fc882ab2abfc69934e428e20147e5
Abner <gedeaoabner@gmail.com>
2021-05-20 15:40:40 -0300

    adiciona testes parte 4

--- bloco_7/testes.js
+++ bloco_7/testes.js
@@ -114,3 +114,20 @@ const customer1 = {
   console.log(listSkillsValuesWithValues(student));
 
   // Testes parte 4
+
+  const países = {
+    França: 'Paris',
+    Brasil: 'Brasília',
+    Espanha: 'Madrid',
+    Portugal: 'Lisboa',
+  };
+  const pairKeyValue = Object.entries(países);
+  console.log(pairKeyValue);
+
+  for(index in pairKeyValue) {
+    console.log('--------');
+    console.log('País:', pairKeyValue[index][0]);
+    console.log('Capital:', pairKeyValue[index][1]);
+  };
+
+  
