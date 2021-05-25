dcc403ff9f95a952e32186618514780063e8e66c
Abner <gedeaoabner@gmail.com>
2021-05-25 15:24:21 -0300

    adiciona exemplo 2

--- bloco_8/exemplo.js
+++ bloco_8/exemplo.js
@@ -1,3 +1,4 @@
+// Início do exemplo 1
 const repeat = (number, action) => {
     for (let count = 0; count <= number; count += 1) {
       action(count);
@@ -6,3 +7,16 @@ const repeat = (number, action) => {
   
   // repeat(5, console.log);
   repeat(5, console.group);
+
+  // Inicio do exemplo 2
+  const repeat = (number, action) => {
+    for (let count = 0; count <= number; count += 1) {
+      action(count);
+    }
+  };
+  
+  repeat(3, (number) => {
+    if (number % 2 === 0) {
+      console.log(number, 'is even');
+    }
+  });
