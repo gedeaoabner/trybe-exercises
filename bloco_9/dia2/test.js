74a3701e997db2ff040225e5daacffa0d4ae4e4e
Abner <gedeaoabner@gmail.com>
2021-06-14 19:03:03 -0300

    iniciando exercicios do dia 1, bloco 10

---
+++ bloco_9/dia2/test.js
@@ -0,0 +1,51 @@
+const promise = new Promise((resolve, reject) => {
+    const number = Math.floor(Math.random() * 11);
+  
+    if (number <= 5) {
+      return reject(console.log(`Que fracasso =( Nosso número foi ${number}`));
+    }
+    resolve(console.log(`Que sucesso =) nosso número foi ${number}`));
+  });
+
+  ///////////////////////////////////////////////////////////////////////////
+
+  const promise = new Promise((resolve, reject) => {
+    const number = Math.floor(Math.random() * 11);
+  
+    if (number <= 5) {
+      return reject(console.log(`Que fracasso =( Nosso número foi ${number}`));
+    }
+    resolve(number);
+  })
+  .then(number => `Que sucesso =) nosso número foi ${number}`)
+  .then(msg => console.log(msg));
+
+  ///////////////////////////////////////////////////////////////////////////
+
+  const promise = new Promise((resolve, reject) => {
+    const number = Math.floor(Math.random()* 11);
+  
+    if (number <= 5) {
+      return reject(number);
+    }
+    resolve(number);
+  })
+  .then(number => `Que sucesso =) nosso número foi ${number}`)
+  .then(msg => console.log(msg))
+  .catch(number => console.log(`Que fracasso =( Nosso número foi ${number}`));
+
+  /////////////////////////////////////////////////////////////////////////////
+
+  const fetch = require('node-fetch');
+
+function verifiedFetch(url) {
+  return new Promise((resolve, reject) => {
+    if (url === 'https://api.chucknorris.io/jokes/random?category=dev') {
+      fetch(url)
+        .then((r) => r.json())
+        .then((r) => resolve(r.value));
+    } else {
+      reject(new Error('endpoint não existe'));
+    }
+  });
+}
